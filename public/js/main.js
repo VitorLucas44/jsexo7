052d11282fa5d4628f06d93d02d873b6326d294b
VitorLucas44 <vitoriosolucas@hotmail.com>
2022-12-20 13:31:27 +0100

    finis

---
+++ public/js/main.js
@@ -0,0 +1,82 @@
+// exo1 et 2
+
+// console.log(1==1)
+// console.log(1===1)
+
+
+
+
+// exo3
+
+// let prenom = prompt("quel es tu prenom ?")
+// if (prenom.length < 5) {
+//     alert("votre prenom est trop court ")
+    
+// } else {
+//     alert("merci à vous")
+// }
+
+
+
+// exo4
+
+// let calcul = prompt("combien font 2+3?")
+// if (calcul==5){
+//     alert("Félicitation, c'est la bonne réponse")
+// } else {
+//         alert("C'est faux !")
+// }
+
+
+
+
+
+// exo5
+
+// let calcul = prompt("combien font 2x3?")
+// if (calcul==6){
+//     alert("Félicitation, c'est la bonne réponse")
+// } else {
+//     alert("C'est faux !")
+// }
+
+
+
+// exo6
+// let tab = [];
+// while (true) {
+// let element = prompt("Entrez un élément à ajouter");
+// tab.push(element);
+//  if (tab.length === 3) {
+//     alert(`Vous avez tapez les éléments suivants : ${tab}`);
+//     break;
+//   }
+// }
+
+
+
+// exo7
+// let array = [];
+// let array2 = [];
+// while (true) {
+// let element = prompt("Entrez un chiffre");
+// array.push(element);
+//  if (array.Math >= 12) {
+//     alert(`tableau1 : ${array}`);
+//     break;
+//   }
+//   else (array2.Math <= 12)
+//   alert(`tableau2 : ${array2}`);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
